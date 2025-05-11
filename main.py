065a40ed0e367bf624451f9875f4f0713af0bbcc
Nebojsa Dimic <nsd4@rice.edu>
2025-05-11 21:27:03 +0200

    Added .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 
-if __name__ == '__main__':
-    print('PyCharm')
+import os
+from dotenv import load_dotenv
+from getpass import getpass
+load_dotenv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or getpass(
+      "Enter OpenAI API Key: "
+)
+
+
+
+
+if __name__ == '__main__':
+    print('Start')
+    print(os.environ['OPENAI_API_KEY'])
+
+
+
